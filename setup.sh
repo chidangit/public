1f0766ac466a3c73553634a39ae02e909efeb273
chidangit <chidangit@gmail.com>
2024-02-21 12:49:40 +0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Kiểm tra và cài đặt Docker nếu chưa được cài đặt
+if ! command -v docker &> /dev/null
+then
+    echo "Docker chưa được cài đặt. Bắt đầu quá trình cài đặt..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    rm get-docker.sh
+    echo "Cài đặt Docker hoàn tất."
+else
+    echo "Docker đã được cài đặt."
+fi
+
+# Kiểm tra và cài đặt Git nếu chưa được cài đặt
+if ! command -v git &> /dev/null
+then
+    echo "Git chưa được cài đặt. Bắt đầu quá trình cài đặt..."
+    sudo apt update
+    sudo apt install git -y
+    echo "Cài đặt Git hoàn tất."
+else
+    echo "Git đã được cài đặt."
+fi
+
+# Xác nhận việc tạo người dùng mới
+read -p "Bạn có muốn tạo người dùng mới không? (yes/no): " create_new_user
+if [ "$create_new_user" == "yes" ]; then
+    read -p "Nhập tên người dùng mới: " new_username
+    sudo adduser $new_username
+    sudo usermod -aG sudo $new_username
+    echo "Người dùng mới đã được tạo và được gán quyền root."
+else
+    echo "Không tạo người dùng mới."
+fi
