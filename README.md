4d3c7cac8355d360c1c6dcb510ea3a6dc02f2ac5
chidangit <chidangit@gmail.com>
2024-02-22 08:43:30 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # public
+
+```
+wget https://raw.githubusercontent.com/chidangit/public/main/setup.sh
+chmod +x setup.sh
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Thay đổi DNS server
+echo "Thay đổi DNS server về 8.8.8.8..."
+sudo echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
 # Kiểm tra và cài đặt Docker nếu chưa được cài đặt
 if ! command -v docker &> /dev/null
 then
@@ -25,8 +29,8 @@ else
 fi
 
 # Xác nhận việc tạo người dùng mới
-read -p "Bạn có muốn tạo người dùng mới không? (yes/no): " create_new_user
-if [ "$create_new_user" == "yes" ]; then
+read -p "Bạn có muốn tạo người dùng mới không? (y/n): " create_new_user
+if [ "$create_new_user" == "y" ]; then
     read -p "Nhập tên người dùng mới: " new_username
     sudo adduser $new_username
     sudo usermod -aG sudo $new_username
